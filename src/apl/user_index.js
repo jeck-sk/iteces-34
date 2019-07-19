10cde9d4bc07366bf4921263d304a572238d5d48
Administrator <742094930@qq.com>
2019-07-19 20:51:12 +0800

    ‘03.登录跳转完成

--- src/apl/user_index.js
+++ src/apl/user_index.js
@@ -1,9 +1,15 @@
 // 引入axios
 import axios from '@/untils/myaxios.js'
+// 登录
 export const login = (data) => {
+  // axios（{})返回一个promise对象
   return axios({
+    //   http://localhost:8888/api/private/v1/login
+    //   请求路由路径
     url: 'login',
+    // 设置请求方式，默认给get
     method: 'post',
+    // post设置
     data
   })
 }
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="home">home</div>
+    <div class="home">首页</div>
 </template>
 <script>
 export default {
